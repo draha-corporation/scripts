722e35f98c8de05debf53a5ed6f675c78041645c
rishith-poloju <rishith.poloju@draha.me>
2023-06-19 02:46:00 +0530

    network security group

--- azure/virtual-machines/create-virtual-machine.sh
+++ azure/virtual-machines/create-virtual-machine.sh
@@ -9,6 +9,7 @@ fi
 
 NODE_NAME=$1
 RESOURCE_GROUP_NAME=$2
+NETWORK_SECURITY_GROUP_NAME="${NODE_NAME}-network-security-group"
 SSH_ALIAS=$3
 IS_CONTROL_PLANE=${4:-false}
 LOCATION="westeurope"
@@ -23,6 +24,12 @@ delete_virtual_machine() {
     az vm delete --resource-group $RESOURCE_GROUP_NAME --name $NODE_NAME --yes
 }
 
+# Function to delete the network security group
+delete_network_security_group() {
+    echo "Deleting network security group $NETWORK_SECURITY_GROUP_NAME..."
+    az network nsg delete --resource-group $RESOURCE_GROUP_NAME --name $NETWORK_SECURITY_GROUP_NAME
+}
+
 # Function to delete the resource group
 delete_resource_group() {
     echo "Deleting resource group $RESOURCE_GROUP_NAME..."
@@ -35,21 +42,39 @@ create_resource_group() {
     az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
 }
 
+# Function to create the network security group
+create_network_security_group() {
+    echo "Creating network security group $NETWORK_SECURITY_GROUP_NAME..."
+    az network nsg create --resource-group $RESOURCE_GROUP_NAME --name $NETWORK_SECURITY_GROUP_NAME --location $LOCATION
+}
+
+# Function to configure inbound port rules for network security group
+configure_port_rules() {
+    echo "Configuring port rules for network security group $NETWORK_SECURITY_GROUP_NAME..."
+
+    if [ "$IS_CONTROL_PLANE" = true ]; then
+        PORT_RANGES=("6443 100" "2379-2380 200" "10250 300" "10259 400" "10257 500")
+    else
+        PORT_RANGES=("10250 100" "30000-32767 200")
+    fi
+
+    for port_range in "${PORT_RANGES[@]}"; do
+        IFS=" " read -r range priority <<< "$port_range"
+        az network nsg rule create --resource-group $RESOURCE_GROUP_NAME --nsg-name $NETWORK_SECURITY_GROUP_NAME \
+            --name "Allow-Inbound-TCP-$range" --priority $priority --direction Inbound --access Allow --protocol Tcp \
+            --destination-port-range $range
+    done
+}
+
 # Function to create the virtual machine
 create_virtual_machine() {
     echo "Creating virtual machine $NODE_NAME..."
-
-    if [ "$IS_CONTROL_PLANE" = true ]; then
-        PORT_RANGES="6443,2379-2380,10250,10259,10257"
-    else
-        PORT_RANGES="10250,30000-32767"
-    fi
-
+    
     az vm create --resource-group $RESOURCE_GROUP_NAME --name $NODE_NAME \
         --size $VM_SIZE --image $VM_IMAGE --location $LOCATION \
         --subscription "$SUBSCRIPTION" --admin-username $ADMIN_USERNAME \
-        --generate-ssh-keys --public-ip-sku Standard \
-        --nsg-rule "PortRanges=$PORT_RANGES;Protocol=TCP"
+        --nsg $NETWORK_SECURITY_GROUP_NAME --public-ip-sku Standard \
+        --generate-ssh-keys
 }
 
 # Function to get the VM IP address
@@ -74,6 +99,14 @@ if [ -n "$VIRTUAL_MACHINE_ID" ]; then
     delete_virtual_machine
 fi
 
+# Check if the network security group exists
+NETWORK_SECURITY_GROUP_ID=$(az network nsg show --resource-group $RESOURCE_GROUP_NAME --name $NETWORK_SECURITY_GROUP_NAME --query id --output tsv 2>/dev/null)
+
+if [ -n "$NETWORK_SECURITY_GROUP_ID" ]; then
+    echo "Network Security Group $NETWORK_SECURITY_GROUP_NAME exists."
+    delete_network_security_group
+fi
+
 # Check if the resource group exists
 RESOURCE_GROUP_ID=$(az group show --name $RESOURCE_GROUP_NAME --query id --output tsv 2>/dev/null)
 
@@ -83,6 +116,8 @@ if [ -n "$RESOURCE_GROUP_ID" ]; then
 fi
 
 create_resource_group
+create_network_security_group
+configure_port_rules
 create_virtual_machine
 get_virtual_machine_ip
 add_ssh_alias
